a1aecef56741e37271f9dd9a6f11bce03108c845
Mandeep Punia <48849295+Mandeep08punia@users.noreply.github.com>
2021-07-09 19:29:32 +0530

    fixed product description

--- src/products/IndividualProduct.js
+++ src/products/IndividualProduct.js
@@ -5,21 +5,21 @@ import useStyles from './individualprodstyles';
 
 function IndividualProduct({product}) {
     const classes=useStyles();
+    console.log(product);
+   
     return (
         <Card className={classes.root}>
-            <CardMedia className={classes.media} image={product.image} title={product.name}/>
+            <CardMedia className={classes.media} image={product.media.source} title={product.name}/>
             <CardContent>
                 <div className={classes.CardContent}>
                     <Typography variant="h6" gutterBottom>
                         {product.name}
                     </Typography>
                     <Typography variant="h5" >
-                        {product.price}
+                        {product.price.formatted_with_code}
                     </Typography>
                 </div>
-                <Typography variant="body2" color="textSecondary">
-                    {product.description}
-                </Typography>
+                <Typography dangerouslySetInnerHTML={{__html:product.description}} variant="body2" color="textSecondary"/>
             </CardContent>
             <CardActions disableSpacing className={classes.CardActions}>
                 <IconButton aria-label="Add to Card">
